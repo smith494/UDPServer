95b666ce6c8e0c504a35363826394a44c9fd8cd6
smith494 <court14104@gmail.com>
2022-12-28 21:39:33 -0600

    End of the UDP discussion

--- src/com/courtneysmith/Main.java
+++ src/com/courtneysmith/Main.java
@@ -3,6 +3,7 @@ package com.courtneysmith;
 import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
+import java.net.InetAddress;
 import java.net.SocketException;
 
 public class Main {
@@ -15,6 +16,13 @@ public class Main {
                 DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
                 socket.receive(packet);
                 System.out.println("Text received is: " + new String(buffer, 0, packet.getLength()));
+
+                String returnString = "echo: " + new String(buffer, 0, packet.getLength());
+                byte [] buffer2 = returnString.getBytes();
+                InetAddress address = packet.getAddress();
+                int port = packet.getPort();
+                packet = new DatagramPacket(buffer2, buffer2.length, address, port);
+                socket.send(packet);
             }
         }catch (SocketException e){
             System.out.println("Socket Exception: " + e.getMessage());
